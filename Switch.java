4c2d6ab617205cd11ad33ea939193a2296457870
PixelPirate07 <pandeydhairya02@gmail.com>
2025-01-16 15:54:58 +0530

    committed

--- Switch.java
+++ Switch.java
@@ -5,10 +5,10 @@ public class Switch {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the first number: ");
-        int num1 = sc.nextInt();
+        int n1 = sc.nextInt();
 
         System.out.print("Enter the second number: ");
-        int num2 = sc.nextInt();
+        int n2 = sc.nextInt();
 
         System.out.print("Enter the operation (+ for addition, - for subtraction): ");
         char operation = sc.next().charAt(0);
@@ -17,11 +17,11 @@ public class Switch {
 
         switch (operation) {
             case '+':
-                result = num1 + num2;
+                result = n1 + n2;
                 System.out.println("The sum is: " + result);
                 break;
             case '-':
-                result = num1 - num2;
+                result = n1 - n2;
                 System.out.println("The difference is: " + result);
                 break;
             default:
